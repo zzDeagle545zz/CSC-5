4ff20bc92d3a1f7f011ae946f19cb32344efd7c4
Christopher Garcia <Deagles545@gmail.com>
2016-01-07 14:59:45 -0800

    Signed-off-by: Christopher Garcia <Deagles545@gmail.com>

---
+++ Hwk/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+
+/* 
+ * File:   main.cpp
+ * Author: zzDeagle545zz
+ *Purpose: Say hello World
+ * Created on January 7, 2016, 2:25 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
+
