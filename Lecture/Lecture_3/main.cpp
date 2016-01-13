30a10f11614cf19e20c3dfa7246342c2a0d841f8
zzDeagle545zz <deagles545@gmail.com>
2016-01-12 19:41:07 -0800

    Signed-off-by: zzDeagle545zz <deagles545@gmail.com>

---
+++ Lecture/Lecture_3/main.cpp
@@ -0,0 +1,76 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *Notes
+ * Created on January 12, 2016, 6:06 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+      //if else statement
+    //if(Condition)
+    //true statement    
+    //else
+    //false statement
+    
+      //if independent
+    //if condition 
+    //chain if statements?
+    
+      //if(condition could compare with "&&" EX (compare 1)&&(compare 2)
+    
+    //logical operators
+    //and operator &&
+    //or operator 
+    //not operator!
+    //avoids nesting
+    //follow order of operations 
+    
+    //{unique vars} starts a new block of code
+    //indent new block
+    
+    //Use block with {} in if statements
+    
+    //Nesting can test multiple statements
+    //INDENT
+    
+    //else if 
+    //if 
+    //else if
+    //etc
+    //else
+    
+    //menu
+    //if/else if
+    
+    //validating data type
+    //-range if else to test data type
+    //scope is variable in a certain block
+    //local block takes precedence over global or higher
+    
+    //comparing strings
+    //strcmp use <string> <cstring>
+
+    //conditional operator 
+    //triary expression    
+    //ex. hours=hours<5?5:hours;
+    
+    //switch statement 
+    //switch(expression)//integar)
+    //case 1
+    //case 2
+    //case 3 etc
+    //needs a break
+    //default 
+    //without break it would be all inclusive and not jump out
+    //menus with switches
+    
+    //test file opening errors
+    
+    return 0;
+}
+
