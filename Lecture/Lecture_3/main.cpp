93c5c886d47618bcfeec0f696f1538a80552e8ea
zzDeagle545zz <deagles545@gmail.com>
2016-01-12 21:45:47 -0800

    Signed-off-by: zzDeagle545zz <deagles545@gmail.com>

--- Lecture/Lecture_3/main.cpp
+++ Lecture/Lecture_3/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
     //ex. hours=hours<5?5:hours;
     
     //switch statement 
-    //switch(expression)//integar)
+    //switch(expression)//integer)
     //case 1
     //case 2
     //case 3 etc
@@ -70,6 +70,49 @@ int main(int argc, char** argv) {
     //menus with switches
     
     //test file opening errors
+    
+    //ternary operator
+    //(score >= a?'A'://else
+    //(score>=b?'A':
+    //...........   :
+    //implements one statement 
+    
+    //if else if has many implements
+    
+    //independent if each statement tests all
+    //Needs a range for each one
+     
+    //switch
+    //cant use ranges
+    //gotta think
+    
+    //logic operators
+    //xor has to be different for true value
+    //xor sawp
+    //a=a^b
+    //b=a^b
+    //a=a^b
+    
+    //works....number swap
+    int a,b;
+    cin>>a>>b;
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    cout<<a<<" "<<b<<endl;
+    
+    //encryptions 
+    //use the truth table
+    //x^y   x^y^y gets x
+    
+    //truth table(Lab))
+    // use ternary operator
+    
+    //<ctime> has time
+    //<csdlib> has rand srand functions
+    //srand(static_cast<unsigned int>)(time(0)))
+    //rand()%61=18/range 18-78
+    
     
     return 0;
 }
